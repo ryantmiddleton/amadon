b0513de2471446ac2d19bd653888ee37362b19af
Ryan Middleton <ryanTmiddleton@gmail.com>
2020-11-01 20:21:24 -0800

    first commit from clone

--- apps/poorly_coded_store/urls.py
+++ apps/poorly_coded_store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^checkout$', views.checkout)
+    url(r'^checkout$', views.checkout),
+    url(r'^store/receipt/(?P<order_id>[0-9]+)$', views.receipt)
 ]
--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
+from django.db.models import Sum
 
 def index(request):
     context = {
@@ -9,8 +10,19 @@ def index(request):
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    price_from_db = float(Product.objects.get(id=request.POST["product_id"]).price)
+    total_charge = quantity_from_form * price_from_db
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    cur_order=Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    return redirect("store/receipt/"+str(cur_order.id))
+
+def receipt(request, order_id):
+    context = {
+        'cur_order':Order.objects.get(id=order_id),
+        'all_orders_num':Order.objects.count(),
+        # Order.objects.aggregate(Sum('total_price')) returns a dictionary {'total_price__sum':Decimal (value)} 
+        # so you must index that dictionary with the 'total_price__sum' key
+        # then round that value to two decimal places
+        'all_orders_sum':round(Order.objects.aggregate(Sum('total_price')) ['total_price__sum'], 2)
+    }
+    return render (request,"store/checkout.html", context)
